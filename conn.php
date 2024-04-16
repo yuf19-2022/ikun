53ea9f32364194831d374c5ab44127f4324b0754
yuf19_2022 <yuf19_2022@qq.com>
2024-04-16 19:19:58 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 // Change this to your own configuration
-$link = new mysqli('120.78.5.218','root','123456','cxk');
+$link = new mysqli('120.78.5.218','ikun','123456','ikun');
 mysqli_set_charset($link, 'utf8');
 if ($link->connect_error) {
     die("Failed to connect: " . $link->connect_error);
